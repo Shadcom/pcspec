4f20c2171d7dc31055be7792308a7dc3650090cf
Dima Lenkov <bagjino@gmail.com>
2016-10-12 14:20:50 +0300

    12/10/2016

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,33 @@
-$(function() {
+$(document).ready(function() {
 
 	// Custom JS
+	$(".aside-nav li").click(function() {
+		$(".aside-nav li").removeClass("active");
+		$(this).addClass("active");
+	});
+
+	$(".top-mnu li").click(function() {
+		$(".top-mnu li").removeClass("active");
+		$(this).addClass("active");
+	});
+
+	// Owl Carousel
+
+	 $("#owl-demo").owlCarousel({
+ 
+      
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true
+ 
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+ 
+  });
+
 
 });
